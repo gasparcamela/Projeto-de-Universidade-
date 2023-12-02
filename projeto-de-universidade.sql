e17dfe2cbbdb810017081a6c680de915b344ad2a
gasparcamela <142824532+gasparcamela@users.noreply.github.com>
2023-12-01 21:27:04 -0300

    Add files via upload
    Neste projeto o principal foco foi criação de um Banco de Dados
    Criação de Tabelas
    Criação de Chaves-Primárias e Chaves-Estrangeiras
    E por fim se fez relacionamento entre as tabelas 
    Nota: Não se fez inserção dos dados, é apenas um projeto simples de um Banco de dados.

---
+++ projeto-de-universidade.sql
@@ -0,0 +1,16 @@
+ALTER TABLE `Aluno` DROP FOREIGN KEY `Aluno_fk0`;
+
+ALTER TABLE `Aluno` DROP FOREIGN KEY `Aluno_fk1`;
+
+ALTER TABLE `Professor` DROP FOREIGN KEY `Professor_fk0`;
+
+ALTER TABLE `Professor` DROP FOREIGN KEY `Professor_fk1`;
+
+ALTER TABLE `Matricula` DROP FOREIGN KEY `Matricula_fk0`;
+
+ALTER TABLE `Disciplina` DROP FOREIGN KEY `Disciplina_fk0`;
+
+ALTER TABLE `Pré-Requisito` DROP FOREIGN KEY `Pré-Requisito_fk0`;
+
+ALTER TABLE `Pré-Requisito` DROP FOREIGN KEY `Pré-Requisito_fk1`;
+
